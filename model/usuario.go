77ec01571c55d6f91a85ceaeaf5b5e3e00715e63
Lucas Duete <lm.duete@gmail.com>
2018-03-25 20:00:11 -0300

    Adicionando Campo username em Usuario.

--- model/usuario.go
+++ model/usuario.go
@@ -3,8 +3,9 @@ package model
 import "gopkg.in/mgo.v2/bson"
 
 type User struct {
-	ID    bson.ObjectId `bson:_id json:id`
-	Nome  string        `bson:nome json:nome`
-	Email string        `bson:email json:email`
-	Senha string        `bson:senha json:senha`
+	ID       bson.ObjectId `bson:_id json:id`
+	Nome     string        `bson:nome json:nome`
+	Username string        `bson:username json:username`
+	Email    string        `bson:email json:email`
+	Senha    string        `bson:senha json:senha`
 }
