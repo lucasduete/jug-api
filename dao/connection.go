5b4f1e34720bef29b242ba795d7b4753ed5d13dd
Lucas Duete <lm.duete@gmail.com>
2018-03-25 20:26:56 -0300

    Adicionando Dependencias ao GoVendor e Removendo Import desnecessário em connection.go

--- dao/connection.go
+++ dao/connection.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"github.com/go-gl/gl/all-core/gl"
 )
 
 const (
---
+++ dao/daoPostgres/usuarioDaoPostgres.go
@@ -0,0 +1,16 @@
+package daoPostgres
+
+import (
+	"jug-api/model"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type userDaoPostgres struct {
+}
+
+
+func (dao *userDaoPostgres) Salvar(user model.User) error {
+
+}
