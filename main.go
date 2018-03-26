6a58f5dbc7dac14304cee702a53abbcd5b3f8a07
Lucas Duete <lm.duete@gmail.com>
2018-03-26 10:13:10 -0300

    Iniciando Implemetação dos EndPoints

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"os"
+	"github.com/gorilla/mux"
+	"net/http"
+	"fmt"
+)
+
 func main() {
+	port := os.Getenv("PORT")
+	router := mux.NewRouter()
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router.HandleFunc("/", notFound).Methods("GET")
+
+	fmt.Println("Servidor Rodando na Porta " + port)
+	http.ListenAndServe(":"+port, router)
+}
+
+func notFound(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(response, "ERROUUUUUU")
 }
