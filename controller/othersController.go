257815915ce2032e76fab74789313083083c1f72
Lucas Duete <lm.duete@gmail.com>
2018-03-26 10:55:47 -0300

    Implementando Métodos de Response Usando Json

--- controller/othersController.go
+++ controller/othersController.go
@@ -3,8 +3,22 @@ package controller
 import (
 	"net/http"
 	"fmt"
+	"encoding/json"
 )
 
 func (app *App) NotFound(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(response, "ERROUUUUUU")
 }
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
