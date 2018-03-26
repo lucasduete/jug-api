94658a2c3734df2d2bbd0ea52a7e908e3390c2ec
Lucas Duete <lm.duete@gmail.com>
2018-03-26 16:35:56 -0300

    Implementando Controller para Respostas

---
+++ controller/responseController.go
@@ -0,0 +1,99 @@
+package controller
+
+import (
+	"net/http"
+	"jug-api/dao/daoMongo"
+	"encoding/json"
+	"jug-api/model"
+	"github.com/gorilla/mux"
+	"strconv"
+)
+
+func (app *App) SalvarResposta(response http.ResponseWriter, request *http.Request) {
+	resp := model.Response{}
+
+	if err := json.NewDecoder(request.Body).Decode(&resp); err != nil {
+		respondWithMessage(response, 400, "Resposta Inválida")
+		return
+	}
+
+	dao := daoMongo.ResponseDaoMongo{}
+	err := dao.Salvar(resp)
+
+	if err != nil {
+		respondWithMessage(response, 500, "Erro ao Cadastrar Resposta")
+	} else {
+		respondWithMessage(response, 200, "Resposta Cadastrada com Sucesso")
+	}
+}
+
+func (app *App) AtualizarResposta(response http.ResponseWriter, request *http.Request) {
+	resp := model.Response{}
+
+	if err := json.NewDecoder(request.Body).Decode(&resp); err != nil {
+		respondWithMessage(response, 400, "Resposta Inválida")
+	}
+
+	dao := daoMongo.ResponseDaoMongo{}
+	err := dao.Atualizar(resp)
+
+	if err != nil {
+		respondWithMessage(response, 500, "Erro ao Atualizar Resposta")
+	} else {
+		respondWithMessage(response, 200, "Resposta Atualizada com Sucesso")
+	}
+}
+
+func (app *App) RemoverResposta(response http.ResponseWriter, request *http.Request) {
+	resp := model.Response{}
+
+	if err := json.NewDecoder(request.Body).Decode(&resp); err != nil {
+		respondWithMessage(response, 400, "Resposta Inválida")
+		return
+	}
+
+	dao := daoMongo.ResponseDaoMongo{}
+	err := dao.Remover(resp)
+
+	if err != nil {
+		respondWithMessage(response, 500, "Erro ao Remover Resposta")
+	} else {
+		respondWithMessage(response, 200, "Resposta Removida com Sucesso")
+	}
+}
+
+func (app *App) ListarRespostas(response http.ResponseWriter, request *http.Request) {
+
+	dao := daoMongo.ResponseDaoMongo{}
+	resps, err := dao.Listar()
+
+	if err != nil {
+		respondWithMessage(response, 500, "Erro ao Recuperar Respostas")
+	} else if len(resps) == 0 {
+		respondWithMessage(response, 204, "Não Há Respostas Cadastradas")
+	} else {
+		respondWithJSON(response, 200, resps)
+	}
+}
+
+func (app *App) GetRespById(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil || id <= 0 {
+		respondWithMessage(response, 400, "ID Inválido")
+	}
+
+	resp := model.Response{}
+	dao := daoMongo.ResponseDaoMongo{}
+
+	err = dao.GetRespById(id, resp)
+
+	if err != nil {
+		respondWithMessage(response, 500, "Erro ao Recuperar Respostas")
+	} else if resp.Conteudo == "" {
+		respondWithMessage(response, 204, "Não Há Respostas Cadastradas")
+	} else {
+		respondWithJSON(response, 200, resp)
+	}
+}
