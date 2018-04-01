eeb8292a52fd11a7f7ae5f8f4c7820cb0e72ec43
Lucas Duete <lm.duete@gmail.com>
2018-04-01 20:38:08 -0300

    Corrigindo Erro no jwtController

--- infoSecurity/jwtController.go
+++ infoSecurity/jwtController.go
@@ -9,7 +9,7 @@ var secret = []byte("S3CR3t$K3Y_F0R_4p1_%JwT%_JUG-4p1")
 func ValidateToken(myToken string) (bool, string) {
 	token, err := jwt.ParseWithClaims(myToken, &ApiClaims{},
 		func(token *jwt.Token) (interface{}, error) {
-			return []byte(mySigningKey), nil
+			return []byte(secret), nil
 		})
 
 	if err != nil {
