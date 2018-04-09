38d2625b095099e0d7b1abfa8364e633fc8e8129
Lucas Duete <lm.duete@gmail.com>
2018-04-08 22:09:08 -0300

    Refatorado models de tecnologia e publicação.

--- model/publicacao.go
+++ model/publicacao.go
@@ -8,9 +8,9 @@ import (
 
 type Publication struct {
 	ID         bson.ObjectId `bson:_id json:id`
-	Titulo     string        `bson:titulo json:titulo`
-	Conteudo   string        `bson:conteudo json:conteudo`
-	Data       time.Time     `bson:data json:data`
+	Titulo     string        `bson:titulo json:"titulo"`
+	Conteudo   string        `bson:conteudo json:"conteudo"`
+	Data       time.Time     `bson:data json:"data"`
 	EmailUser  string        `bson:emailUser json:"-"`
-	Tecnologia string        `bson:tecnologia json:tecnologia`
+	Tecnologia string        `bson:tecnologia json:"tecnologia"`
 }
--- model/tecnologia.go
+++ model/tecnologia.go
@@ -3,6 +3,6 @@ package model
 import "gopkg.in/mgo.v2/bson"
 
 type Tecnology struct {
-	ID   bson.ObjectId `bson:_id json:id`
-	Nome string        `bson:nome json:nome`
+	ID   bson.ObjectId `bson:_id json:"id"`
+	Nome string        `bson:nome json:"nome"`
 }
