2f2b813dd956bddb53fe995066b7aca673b0657e
Lucas Duete <lm.duete@gmail.com>
2018-04-08 21:55:52 -0300

    Refatorando usuario model.

--- model/usuario.go
+++ model/usuario.go
@@ -1,11 +1,8 @@
 package model
 
-import "gopkg.in/mgo.v2/bson"
-
 type User struct {
-	ID       bson.ObjectId `bson:_id json:id`
-	Nome     string        `bson:nome json:nome`
-	Username string        `bson:username json:username`
-	Email    string        `bson:email json:email`
-	Senha    string        `bson:senha json:senha`
+	Nome     string `bson:nome json:"nome"`
+	Username string `bson:username json:"username"`
+	Email    string `bson:email json:"email"`
+	Senha    string `bson:senha json:"senha"`
 }
