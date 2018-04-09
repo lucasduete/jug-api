548a42aa68bbbe351471afb0769552e5b1701353
Lucas Duete <lm.duete@gmail.com>
2018-04-09 02:33:41 -0300

    Implementando Filtro CORS

---
+++ infraSecurity/corsFilter.go
@@ -0,0 +1,15 @@
+package infraSecurity
+
+import "github.com/rs/cors"
+
+func CorsFilter() *cors.Cors {
+	corsFilter := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://foo.com"},
+		AllowedMethods : []string{"POST", "GET", "PATCH", "DELETE", "OPTIONS"},
+		AllowCredentials: true,
+		AllowedHeaders : []string{"Access-Control-Allow-Headers", "accept, origin, X-Requested-With, Content-Type, X-Codingpedia, Authorization"},
+		MaxAge : 172800,
+	})
+
+	return corsFilter
+}
