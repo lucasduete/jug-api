11ce9d9c30b806f18b8d1243b814b8f857cd8669
Lucas Duete <lm.duete@gmail.com>
2018-04-11 09:17:12 -0300

    Corrigindo metodo da rota de busca de publicação

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func main() {
 	router.HandleFunc(url_base+"publications/", app.ListarPublications).Methods("GET")
 	router.HandleFunc(url_base+"publications/{idPublication}", app.GetPublById).Methods("GET")
 	router.HandleFunc(url_base+"publications/tecnology/", app.GetPublsByTec).Methods("POST")
-	router.HandleFunc(url_base+"publications/search/", app.GetPublsByIndice).Methods("GET")
+	router.HandleFunc(url_base+"publications/search/", app.GetPublsByIndice).Methods("POST")
 	router.HandleFunc(url_base+"publications/recomendation/{idPublication}", app.GetRecomendation).Methods("GET")
 
 	//Defaults EndPoints
